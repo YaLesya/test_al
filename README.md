b2c72110ee62899dfd77d3d6e925e4a2412adc18
Lesya Yavtushenko <jalesya@gmail.com>
2016-02-21 19:56:56 +0300

    test add

---
+++ README.md
@@ -0,0 +1,6 @@
+Это тестовый проект.
+Как установить:
+git clone https://github.com/YaLesya/test_al
+bower i
+npm i
+gulp
---
+++ app/js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+ 
+  $("#owl-example").owlCarousel({
+ 
+      autoPlay: 3000, //Set AutoPlay to 3 seconds
+ 
+      items : 4,
+      itemsDesktop : [1199,3],
+      itemsDesktopSmall : [979,3]
+ 
+  });
+ 
+   $("#owl-example2").owlCarousel({
+ 
+      autoPlay: 3000, //Set AutoPlay to 3 seconds
+ 
+      items : 3,
+      itemsDesktop : [1199,3],
+      itemsDesktopSmall : [979,3]
+ 
+  });
+});
+
